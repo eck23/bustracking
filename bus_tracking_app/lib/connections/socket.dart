d231d0f5872903bb472dcccab888c478924e16f1
Elson C Kaithamangalam <62635566+eck23@users.noreply.github.com>
2022-07-23 11:48:27 +0530

    Add files via upload

---
+++ bus_tracking_app/lib/connections/socket.dart
@@ -0,0 +1,28 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+import '../authentication/authfunctions.dart';
+import '../main.dart';
+
+late IO.Socket socket;
+void connect() {
+  socket = IO.io(url, <String, dynamic>{
+    "transports": ["websocket"],
+    "autoConnect": false
+  });
+
+  socket.connect();
+
+  socket.onConnect((data) {
+    print("flutter connected to socket");
+
+    socket.on('/message', (data) => print(data));
+    socket.on('/datachange', (data) => print(data));
+    socket.on('/filterstop', (data) {
+      if (data != null) {
+        stopProvider.stopsSearchList = data;
+      }
+    });
+  });
+
+  socket.emit('/listenDB', "hello im here");
+}
